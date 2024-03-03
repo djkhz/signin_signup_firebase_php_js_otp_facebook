ed6668ccd4a3a5f3ee99cedaf6aec6cda72a2d1f
djkhz <zheezs@hotmail.com>
2024-03-03 11:48:16 +0700

    Update sign.php

--- sign.php
+++ sign.php
@@ -372,6 +372,10 @@
 <script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-auth.js"></script>
 
 <script>
+  import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries\
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
