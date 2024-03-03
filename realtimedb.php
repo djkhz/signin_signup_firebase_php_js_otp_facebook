bf92eda0902a1fdcd9a3406a6c6dab2870cde473
djkhz <zheezs@hotmail.com>
2024-03-03 12:09:46 +0700

    Create realtimedb.php

---
+++ realtimedb.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+// This assumes that you have placed the Firebase credentials in the same directory
+// as this PHP file.
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/google-service-account.json');
+
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+    ->create();
+
+$database = $firebase->getDatabase();
+
+$newPost = $database
+    ->getReference('blog/posts')
+    ->push([
+        'title' => 'Post title',
+        'body' => 'This should probably be longer.'
+    ]);
+
+$newPost->getChild('title')->set('Changed post title');
+$newPost->getValue(); // Fetches the data from the realtime database
+$newPost->remove();
