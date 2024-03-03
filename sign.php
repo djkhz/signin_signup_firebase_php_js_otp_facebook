2548d04924f5049af117337a8443957259689399
djkhz <zheezs@hotmail.com>
2024-03-03 11:55:03 +0700

    Update sign.php

--- sign.php
+++ sign.php
@@ -372,8 +372,8 @@
 <script src="https://www.gstatic.com/firebasejs/8.2.4/firebase-auth.js"></script>
 
 <script>
-  import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//   import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries\
   // Your web app's Firebase configuration
